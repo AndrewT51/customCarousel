20756455705112c689af9f4778cf8decb69fa072
Andrew Trigg <andrewt51@hotmail.com>
2015-12-25 02:11:50 +0000

    Carousel works properly after a few clicks left

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 $(document).ready(init);
 var focusedItemDetails;
 var slider = -15;
+var negatives = false;
+var reversedArr = objArray.slice();
+reversedArr.reverse();
+var reverse = false;
 
 function init(){
   displayArray();
@@ -9,13 +13,26 @@ function init(){
 }
 
 function displayArray(){
-  objArray.forEach(function(obj){
-    $(".picArray").append($("<img src='"+ obj.url+"' class='unfocused'>"))
-    if(obj.focus){
-      focusedItemDetails = obj; 
-      $("img[src$='"+obj.url+"']").addClass('focused').removeClass('unfocused');
-    }
-  })
+  if(!reverse){
+    objArray.forEach(function(obj){
+      $(".picArray").append($("<img src='"+ obj.url+"' class='unfocused'>"))
+      if(obj.focus){
+        focusedItemDetails = obj; 
+        $("img[src$='"+obj.url+"']").addClass('focused').removeClass('unfocused');
+      }
+    })
+  }else{
+
+    reversedArr.forEach(function(obj){
+      $(".picArray").prepend($("<img src='"+ obj.url+"' class='unfocused'>"))
+      if(obj.focus){
+        focusedItemDetails = obj; 
+        $("img[src$='"+obj.url+"']").addClass('focused').removeClass('unfocused');
+        console.log("here:",focusedItemDetails)
+      }
+    })
+    reverse=false;
+  }
 }
 
 function displayMainFocus(){
@@ -25,8 +42,9 @@ function displayMainFocus(){
 
 function navigationControl(){
   $(".next").on("click",function(){
-    slider -= 30;
 
+
+    slider -= 30;
     $("img[src$='"+focusedItemDetails.url+"']").removeClass('focused').addClass('unfocused');
     $(".picArray img").css("left",slider+"%")
 
@@ -36,8 +54,10 @@ function navigationControl(){
       focusedItemDetails = objArray[focusedItemDetails.id + 1]
       focusedItemDetails.focus = true;
     }else{
+      slider += 150;
       focusedItemDetails.focus = false;
       focusedItemDetails = objArray[0]
+      console.log('addsHere')
       focusedItemDetails.focus = true;
     }
 
@@ -49,16 +69,21 @@ function navigationControl(){
 
   $(".previous").on("click",function(){
     slider += 30;
-    $("img[src$='"+focusedItemDetails.url+"']").removeClass('focused');
+    $("img[src$='"+focusedItemDetails.url+"']").removeClass('focused').addClass('unfocused');
     $(".picArray img").css("left",slider+"%")
-    if(objArray[focusedItemDetails.id].id - 1 >= 1){
+    if(objArray[focusedItemDetails.id].id -1  >= 0){
       focusedItemDetails.focus = false;
       focusedItemDetails = objArray[focusedItemDetails.id - 1]
       focusedItemDetails.focus = true;
     }else{
+      slider -= 300;
       focusedItemDetails.focus = false;
+
+      objArray[objArray.length-1].focus = true;
       focusedItemDetails = objArray[objArray.length-1]
-      focusedItemDetails.focus = true;
+      
+      console.log(focusedItemDetails)
+      reverse = true;
     }
     $("#picHolder").empty();
     $("#textContainer p").empty();
